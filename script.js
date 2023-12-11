73272d27ca422bcd947d4dfd0db228210d0232ee
Ion Mironov <mironov.ion@gmail.com>
2023-12-11 10:17:21 -0500

    Style changes

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const portfolioSection = document.getElementById("portfolio");
+	const aboutSection = document.getElementById("aboutContent");
+	const aboutContent = `
+		<p>I am an IT professional with a rich career spanning over 15 years as a Senior IT Field Technician. I pride myself on my strong troubleshooting and analytical skills, strengthened through years of hands-on experience.</p>
+		<p>When I'm not working, my attention tends to divert to automotive technology, particularly in creating custom car instrument panels and UI displays. This hobby allows me to blend my technical skills with some style, resulting in unique and functional automotive enhancements. My spare time often involves designing and fabricating custom car parts, integrating LEDs, and developing control panel UIs from scratch.</p>
+	`;
+
+    aboutSection.innerHTML = aboutContent;
+
+
+	const projectsSection = document.getElementById("projects");
 	const projects = [
 		{ name: "Car Instrument Panel", description: "A custom car instrument panel I designed which I run on my Raspberry Pi 4 with touchscreen, all of which plugs into a car's OBD-II port. Graphics were created in Adobe Illustrator and coding was done in VS Code. Utilizes Python, PySide6, pyOBD, Qt, and QML.", link: "https://github.com/Ion-Mironov/Instrument_Panel" },
 		{ name: "LED Control Panel", description: "A touchscreen UI created to select various animations on an LED matrix; used for testing purposes. I needed something that I could run on my Raspberry Pi and would allow me to bench test the different light animations that I coded in Python.", link: "#" },
@@ -14,6 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
 			<p>${project.description}</p>
 			<a href="${project.link}" target="_blank">View Project</a>
 		`;
-		portfolioSection.appendChild(projectElement);
+		projectsSection.appendChild(projectElement);
 	});
 });
