36768513cd74a287b81f49af214427ffe039edda
Ion Mironov <mironov.ion@gmail.com>
2023-12-28 16:15:51 -0500

    Current changes with new styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
+import { black as _black, white as _white, slate, emerald, violet, amber, fuchsia } from 'tailwindcss/colors'
+
 export const content = [
 	"./src/input.css",
 	"./index.html",
 	"./script.js"
-];
+]
+
 export const theme = {
-	extend: {
-		backgroundSize: {
-			"600px": "600px",
-		}
+	colors: {
+		transparent: 'transparent',
+		current: 'currentColor',
+		black: _black,
+		white: _white,
+		gray: slate,
+		green: emerald,
+		purple: violet,
+		yellow: amber,
+		pink: fuchsia,
 	},
-};
-export const plugins = [];
+
+	screens: {
+		'lg': '1024px'
+	},
+	
+	extend: {},
+}
+
+export const plugins = [
+	require('@tailwindcss/typography')
+]
