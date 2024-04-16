9596e2b0bc89666584bb660355d84fdfafa438bf
Ion Mironov <mironov.ion@gmail.com>
2024-04-16 15:47:43 -0400

    Small edits to About Me

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	// ====== About Content ====== \\
 	const aboutSection = document.getElementById("aboutContent");
 	const aboutContent = /* html */ `
-		<p>I'm a seasoned IT professional with over 15 years of experience, especially as a Senior IT Field Technician, with a strong background of troubleshooting and analytical skills. I'm all about solving tricky tech problems and have a knack for diving deep into puzzling challenges, even if it may test my sanity.</p>
+		<p>I'm a seasoned IT professional with over 15 years of experience as a Senior IT Field Technician, along with a strong background of troubleshooting and analytical skills. I'm all about solving tricky tech problems and have a knack for diving deep into puzzling challenges, even if it may test my sanity.</p>
 
-		<br>Off the clock, I'm a car tech enthusiast and fabricator. I love bringing some flair to automotive tech, using my imagination to design digital car instrument panels and sleek UI displays from scratch. Designing and building custom car parts (from metal, wood, and even plastics), playing with LEDs, and creating control panel UIs from scratch are just some of the ways I combine my tech skills with my passion for cars.
+		<br>Off the clock, I'm a car tech enthusiast and fabricator. I love bringing some flair to automotive tech, using my imagination to design digital car instrument panels and sleek UI displays from scratch. Designing and building custom car parts (from metal, wood, and even plastics), playing with LEDs, and creating control panel UIs are just some of the ways I combine my tech skills with my passion for cars.
 	`;
 
 	aboutSection.innerHTML = aboutContent;
